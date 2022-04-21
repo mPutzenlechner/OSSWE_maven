3424d5aaf4ab16bc6915fad5400e4bb60e7ccd2f
Moritz Putzenlechner <moritz.putzenlechner@hs-furtwangen.de>
2022-04-21 14:39:11 +0200

    added maven project

---
+++ src/main/java/de/hfu/Main.java
@@ -0,0 +1,16 @@
+package de.hfu;
+import java.util.Scanner;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main
+{
+    public static void main(String[] args) {
+        System.out.println("input: ");
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        System.out.println(input.toUpperCase());
+    }
+}
