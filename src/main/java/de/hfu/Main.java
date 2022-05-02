374834d49a633d1917605559ecabad7ef74e38ce
Moritz Putzenlechner <moritz.putzenlechner@hs-furtwangen.de>
2022-05-02 10:46:12 +0200

    added files for ex7

--- src/main/java/de/hfu/Main.java
+++ src/main/java/de/hfu/Main.java
@@ -12,5 +12,6 @@ public class Main
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
         System.out.println(input.toUpperCase());
+        scanner.close();
     }
 }
